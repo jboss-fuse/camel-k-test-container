bcb0582e2fb977d3978deaf7de83a3505c622a21
Jan Bouska <jbouska@redhat.com>
2022-09-15 10:07:59 +0200

    ROSA testing

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,5 @@
-FROM docker.io/golang:1.14
-
-ARG REPO="https://gitlab.cee.redhat.com/jboss-fuse-qe/fuse.next/acceptance-tests.git"
-ARG BRANCH="master"
-
-RUN git clone --single-branch -c http.sslVerify=false --branch ${BRANCH} ${REPO}
+FROM docker.io/golang:1.17
 
 COPY runTest.sh /opt/runTest.sh
-
-RUN mkdir /test-run-results
 
 CMD [ "/opt/runTest.sh" ]
--- runTest.sh
+++ runTest.sh
@@ -1,18 +1,46 @@
 #!/bin/bash
 set -o pipefail
 
-if [ -z "$KAMEL_CLI" ]; then
-  echo "[ERROR] KAMEL_CLI variable is not set."
+KAMEL_URL=https://mirror.openshift.com/pub/openshift-v4/clients/camel-k/${VERSION}/camel-k-client-${VERSION}-linux-64bit.tar.gz
+REPO=https://github.com/apache/camel-k.git
+RESULTS_DIR="${TEST_COLLECT_BASE_DIR:=/data/results}"
+
+fail() {
+  echo "$@" 1>&2
   exit 1
+}
+
+download_cli() {
+  echo "Downloading KAMEL_CLI from ${KAMEL_URL}"
+  curl -s -O ${KAMEL_URL}
+  tar xzf ${KAMEL_URL##*/}
+  export KAMEL_BIN=${PWD}/kamel
+}
+
+clone_testsuite() {
+  echo "Clonning the test suite from $REPO:release-${VERSION%.*}.x"
+  git clone -q --single-branch -c http.sslVerify=false --branch release-${VERSION%.*}.x ${REPO}
+}
+
+run_test() {
+  name="$1"; shift
+  go test -timeout 20m -v -tags=integration $@ 2>&1 | tee ${RESULTS_DIR}/${name}.log
+  go-junit-report < ${RESULTS_DIR}/${name}.log  > ${RESULTS_DIR}/${name}.xml
+}
+
+if [ -z "$VERSION" ]; then
+  fail "[ERROR] VERSION variable is not set."
 fi
 
-echo "Downloading KAMEL_CLI from ${KAMEL_CLI}"
-curl -O ${KAMEL_CLI}
-tar xzf ${KAMEL_CLI##*/}
-export KAMEL_BIN=${PWD}/kamel
+export KAMEL_INSTALL_BUILD_TIMEOUT=15m
+export CAMEL_K_TEST_TIMEOUT_SHORT=5m
+export CAMEL_K_TEST_TIMEOUT_MEDIUM=15m
+export CAMEL_K_TEST_TIMEOUT_LONG=20m
 
-cd acceptance-tests
-go test -v ./test/operator/smoke/... 2>&1 | tee report.txt
+clone_testsuite
+download_cli
+go install github.com/jstemmer/go-junit-report@latest
 
-go get -u github.com/jstemmer/go-junit-report
-go-junit-report <report.txt >/test-run-results/report.xml
+cd camel-k
+run_test common ./e2e/common/rest_test.go
+run_test traits ./e2e/common/traits/route_test.go ./e2e/common/traits/prometheus_test.go
